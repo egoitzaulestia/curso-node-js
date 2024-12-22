1eedbb455d0c1d29b0bbb5c01904b9afe70dcc13
egoitzaulestia <egoitzaulestia@gmail.com>
2024-12-23 00:29:48 +0100

    api pokemon

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,15 +1,35 @@
 const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-    console.log('request received')
-    res.end('Hola mundo')
-})
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+  if (req.url === '/') {
+    // res.statusCode = 200 // OK
+    res.end('<h1>Mi página</h1>')
+  } else if (req.url === '/imagen-grafo.png') {
+    fs.readFile('./graph_supreme.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Sever Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    // res.statusCode = 200 // OK
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404 // Not Found
+    res.end('<h1>404</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-    console.log(`server listening on port http://localhost:${port}`) // por está bien??
+  console.log(`server listening on port http://localhost:${desiredPort}`)
 })
-
-
- 
