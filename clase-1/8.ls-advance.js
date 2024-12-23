f9989a7c68aaad13cebc3787664f7350ea652a3a
egoitzaulestia <egoitzaulestia@gmail.com>
2024-12-23 01:08:06 +0100

    express 02

--- clase-1/8.ls-advance.js
+++ clase-1/8.ls-advance.js
@@ -5,37 +5,36 @@ const pc = require('picocolors')
 const folder = process.argv[2] ?? '.'
 
 async function ls (folder) {
-    let files
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(pc.red(`❌ No se pudo leer el directorio ${folder}`))
+    process.exit(1)
+  }
+
+  const filePromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
+
     try {
-        files = await fs.readdir(folder)
+      stats = await fs.stat(filePath) // status - información del archivo
     } catch {
-        console.error(pc.red(`❌ No se pudo leer el directorio ${folder}`))
-        process.exit(1)
+      console.error(`No se pudo leer el archivo ${filePath}`)
+      process.exit(1)
     }
 
-    const filePromises = files.map(async file => {
-        const filePath = path.join(folder, file)
-        let stats
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size.toString()
+    const fileModified = stats.mtime.toLocaleDateString()
 
-        try {
-            stats = await fs.stat(filePath) // status - información del archivo
-        } catch {
-            console.error(`No se pudo leer el archivo ${filePath}`)
-            process.exit(1)
-        }
+    return `${pc.white(fileType)} ${pc.blue(file.padEnd(34))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
+  })
 
-        const isDirectory = stats.isDirectory()
-        const fileType = isDirectory ? 'd' : 'f'
-        const fileSize = stats.size.toString()
-        const fileModified = stats.mtime.toLocaleDateString()
+  const filesInfo = await Promise.all(filePromises)
 
-        return `${pc.white(fileType)} ${pc.blue(file.padEnd(34))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
-    });;
-
-    const filesInfo = await Promise.all(filePromises)
-
-    filesInfo.forEach(fileInfo => console.log(fileInfo))
+  filesInfo.forEach(fileInfo => console.log(fileInfo))
 }
 
 ls(folder)
-
--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,1 +1,8 @@
-const 
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT ?? 1234
+
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Mi página</h1>')
+})
