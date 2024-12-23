6fa26b67709b073bcb9c07fb40f4f1da91915b24
egoitzaulestia <egoitzaulestia@gmail.com>
2024-12-23 01:00:55 +0100

    express

--- clase-1/9.http.js
+++ clase-1/9.http.js
@@ -6,15 +6,12 @@ console.log(process.env)
 const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
-    console.log('request received')
-    res.end('Hola mundo')
+  console.log('request received')
+  res.end('Hola mundo')
 })
 
 findAvailablePort(desiredPort).then(port => {
-    server.listen(port, () => {
-        console.log(`server listening on port http://localhost:${port}`)
-    })
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
 })
-
-
- 
